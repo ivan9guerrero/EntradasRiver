5e80a3f7460313b388fa6c5a21a22f12161d05f9
ivan9guerrero <ivan9guerrero@gmail.com>
2023-11-12 19:20:32 +0000

    PreEntrega 1

--- preentrega1.js
+++ preentrega1.js
@@ -6,19 +6,28 @@ let entrada;
 let precio;
 let cantidad;
 let total = 0;
-let comprarMas = SI;
+let comprarMas = SI; // Inicializado en 'si' para comenzar el bucle
 let continuar;
 
 function comprar_entrada(mail) {
-
+    let Ubicación;
     let tipo_entrada;
-    let Popular_100 = '1';
-    let Platea_Baja_200 = '2';
-    let Platea_Alta_300 = '3';
-    let Palco_Vip_500 = '4';
+    let categorias = {
+        '1': 'Popular $100',
+        '2': 'Platea Baja $200',
+        '3': 'Platea Alta $300',
+        '4': 'Palco Vip $500'
+    };
     let salir = 'S';
+    let compras = [];
 
-    mail = prompt('Bienvenido a la plataforma de entradas de River Plate\n Ingrese su email: ');
+    // Validar el correo electrónico
+    do {
+        mail = prompt('Bienvenido a la plataforma de entradas de River Plate\n Ingrese su email: ');
+        if (!/\S+@\S+\.\S+/.test(mail)) {
+            alert('Por favor, ingresa un correo electrónico válido.');
+        }
+    } while (!/\S+@\S+\.\S+/.test(mail));
 
     while (comprarMas.toLowerCase() === SI) {
         tipo_entrada = prompt('Elegi la categoría que deseas: ' + '\n' +
@@ -29,29 +38,31 @@ function comprar_entrada(mail) {
             '\n' +
             '¿Queres salir? Tocá S');
 
-        if (
-            tipo_entrada !== Popular_100 &&
-            tipo_entrada !== Platea_Baja_200 &&
-            tipo_entrada !== Platea_Alta_300 &&
-            tipo_entrada !== Palco_Vip_500 &&
-            tipo_entrada !== salir
-        ) {
+        if (!categorias[tipo_entrada] && tipo_entrada !== salir) {
             alert('Ingresaste una categoría que no existe!');
         } else if (tipo_entrada !== salir) {
-            cantidad = parseInt(prompt('Ingrese la cantidad de entradas: '));
+            // Validar que la cantidad ingresada sea un número entero positivo
+            do {
+                cantidad = prompt('Ingrese la cantidad de entradas: ');
+                if (!/^\d+$/.test(cantidad)) {
+                    alert('Por favor, ingresa un número entero positivo para la cantidad de entradas.');
+                }
+            } while (!/^\d+$/.test(cantidad));
 
+            cantidad = parseInt(cantidad);
 
+            // Asumiendo que el precio es fijo para cada categoría
             switch (tipo_entrada) {
-                case Popular_100:
+                case '1':
                     precio = 100;
                     break;
-                case Platea_Baja_200:
+                case '2':
                     precio = 200;
                     break;
-                case Platea_Alta_300:
+                case '3':
                     precio = 300;
                     break;
-                case Palco_Vip_500:
+                case '4':
                     precio = 500;
                     break;
                 default:
@@ -59,12 +70,29 @@ function comprar_entrada(mail) {
             }
             total += precio * cantidad;
 
-            comprarMas = prompt('¿Quieres comprar más entradas? (' + SI + '/' + NO + ')');
+            // Almacenar la información de la compra
+            compras.push({
+                categoria: categorias[tipo_entrada],
+                cantidad: cantidad
+            });
+
+            do {
+                comprarMas = prompt('¿Quieres comprar más entradas? (' + SI + '/' + NO + ')').toLowerCase();
+                if (comprarMas !== SI && comprarMas !== NO) {
+                    alert('Por favor, ingresa "si" o "no".');
+                }
+            } while (comprarMas !== SI && comprarMas !== NO);
         }
     }
 
-    if (comprarMas.toLowerCase() === NO) {
-        alert('Gracias por tu compra. Estas son tus entradas:\nTotal a pagar: $' + total);
+    if (comprarMas === NO) {
+        // Construir el mensaje final detallando las categorías y cantidades de entradas compradas
+        let mensajeFinal = 'Gracias por tu compra. Estas son tus entradas:\n';
+        for (let compra of compras) {
+            mensajeFinal += `${compra.categoria}: ${compra.cantidad} entradas\n`;
+        }
+        mensajeFinal += `Total a pagar: $${total}`;
+        alert(mensajeFinal);
     }
 }
 
